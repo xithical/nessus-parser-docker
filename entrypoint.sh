1aee20c89c47488dd4c276de8e7d89c064367a91
Scott R <86810816+xithical@users.noreply.github.com>
2024-03-15 13:57:33 -0500

    create the initial entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+<<head
+ Most commands in this have their output pointing to /dev/null 
+ so that it's a little easier to grab the resulting output.
+ This helps overcome output size limitations as well in some
+ automation platforms.
+head
+
+# Download Nessus parser script
+wget https://bitbucket.org/!api/2.0/snippets/compuquipteam/Edg9kA/afbfbb235c3fdbfb9950c1d4649545e7990bd659/files/parse_nessus_xml.pl > /dev/null 2>&1
+
+# Get Nessus export from environment variable URL
+wget $NESSUS_EXPORT -O "export.nessus" > /dev/null 2>&1
+
+# Run the Perl parser script
+perl parse_nessus.xml.pl -f export.nessus > /dev/null 2>&1
+
+# Find the XLSX outputm encode it as B64, then store in the $OUTPUT env var
+OUTFILE=$(find . -name "nessus_report*")
+OUTPUT=$(echo -n ./$OUTFILE | base64)
+
+# Echo the resulting output in JSON format
+echo "{\"output\":\"$OUTPUT\"}"
